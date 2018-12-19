2a9ea1582d40951dcab837d41167128c6dd0ec9d
Muhammet Arslan <m.arslan@DEAMWS4314-1063.local>
2018-12-19 11:32:24 +0100

    new k8s module

---
+++ modules/networking/variables.tf
@@ -0,0 +1,26 @@
+#############################################################
+##
+## This app file contains the Variables of Networking of Kubernetes for 
+## HighCloud Kubernetes Terraform Module 
+## 
+## @package /highcloud-k8s/modules/networking/security_group/groups
+## @year 2018
+## @author Muhammet Arslan <muhammet.arslan@highcloud.team>
+##
+#############################################################
+
+variable "cluster_domain" {
+  description = "Application Domain to be used on all the K8S Resources as identifier"
+  default     = ""
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
+
+## VPC Settings
+variable "vpc_id" {
+  description = "VPC ID to locate resources in it"
+  default     = ""
+}
